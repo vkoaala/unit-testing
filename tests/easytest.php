a53e2a98b03dd49d0469b50126e641e877768576
Justin <xbljustin@hotmail.com>
2017-02-10 09:58:01 -0500

    Update easytest.php

--- tests/easytest.php
+++ tests/easytest.php
@@ -1,26 +1,9 @@
 <?php
-/*
- * This file is part of the Money package.
- *
- * (c) Sebastian Bergmann <sebastian@phpunit.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace SebastianBergmann\Money;
-
-use PHPUnit\Framework\TestCase;
-
-class easytest extends TestCase
+class easytest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @covers            \SebastianBergmann\Money\Money::__construct
-     * @uses              \SebastianBergmann\Money\Currency
-     * @expectedException \SebastianBergmann\Money\InvalidArgumentException
-     */
-    public function testExceptionIsRaisedForInvalidConstructorArguments()
+    public function testAssertTrueIsTrue()
     {
-        new Money(null, new Currency('EUR'));
+        $this->assertEquals(true, true);
     }
 }
+?>
